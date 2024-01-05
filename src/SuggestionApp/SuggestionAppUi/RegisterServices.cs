fb9708a06b483177b590d80f49f6fac89a4508aa
Michael Koch <m.koch@neuko-devment.com>
2024-01-05 22:43:03 +0100

    Lession 7

---
+++ src/SuggestionApp/SuggestionAppUi/RegisterServices.cs
@@ -0,0 +1,11 @@
+﻿namespace SuggestionAppUi;
+
+public static class RegisterServices
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddMemoryCache();
+    }
+}
